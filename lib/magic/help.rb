c397f69b9a65e600d519e7e91ac275907f6f0239
Tomasz Wegrzanowski <Tomasz.Wegrzanowski@gmail.com>
2017-01-19 17:02:12 +0000

    Moving code around a bit more

--- lib/magic/help.rb
+++ lib/magic/help.rb
@@ -57,6 +57,168 @@ module Magic
       end
       m
     end
+
+    def self.resolve_help_query(*args, &block)
+      raise ArgumentError, "wrong number of arguments (#{args.size} for 1)" if args.size > 1
+      raise ArgumentError, "help cannot take both arguments and block" if args.size > 0 and block_given?
+
+      if block_given?
+        call_event = nil
+        res = nil
+        done = false
+        res_mm = nil
+        argument_error = false
+        base_level_module = (RUBY_VERSION > '1.9' ? BasicObject : Kernel)
+
+        # We want to capture calls to method_missing too
+        original_method_missing = base_level_module.instance_method(:method_missing)
+        base_level_module.class_eval {
+          def method_missing(*args)
+            throw :magically_irb_helped, [self, *args]
+          end
+        }
+
+        tf = lambda{|*xargs|
+          return if done
+          event = xargs[0]
+          if argument_error
+            if event == 'return'
+              done = true
+              # For functions called with wrong number of arguments,
+              # call event is not generated (function is never called),
+              # but the return event is !
+              call_event = xargs
+              throw :magically_irb_helped
+            end
+          elsif event == 'call' or event == 'c-call'
+            call_event = xargs
+            if call_event.values_at(0, 3, 5) == ['c-call', :new, Class] and
+              eval("self", call_event[4]) == ArgumentError
+              argument_error = true
+            else
+              done = true
+              # Let Kernel#method_missing run, otherwise throw
+              unless call_event.values_at(0, 3, 5) == ['call', :method_missing, base_level_module]
+                throw :magically_irb_helped
+              end
+            end
+          end
+        }
+        res_mm = catch(:magically_irb_helped) {
+          set_trace_func tf
+          res = yield()
+          done = true
+          nil
+        }
+        done = true
+        set_trace_func nil
+        base_level_module.instance_eval {
+          define_method(:method_missing, original_method_missing)
+          # It was originally private, restore it as such
+          private :method_missing
+        }
+        # Handle captured method_missing
+
+        if res_mm
+          # This is explicit Foo#method_missing:
+          # It shouldn't really be allowed as it's private.
+          if res_mm.size == 1
+            return "Kernel#method_missing"
+          else
+            bound_self, meth = *res_mm
+            # method_missing is called if:
+            # * there was no such method
+            # * there was a method, but a private one only
+            begin
+              # Surprise ! A private method !
+              m = bound_self.method(meth)
+              query = help_method_extract(m)
+            rescue NameError
+              # No such method
+              if bound_self.is_a? Class
+                query = "#{bound_self}::#{meth}"
+              else
+                query = "#{bound_self.class}.#{meth}"
+              end
+            end
+          end
+          query = Magic::Help.postprocess(query)
+          return query
+        # Handle normal call events
+        elsif call_event
+          meth, bind, cls = call_event[3], call_event[4], call_event[5]
+          bound_self = eval('self', bind)
+          if meth == :new
+            #puts "Warning: Class.new called: #{call_event.inspect}"
+            #puts "self is #{bound_self}"
+            cls = bound_self
+          end
+
+          # Foo::bar and Foo#bar look the same
+          # Check whether self == Foo to tell them apart
+          #
+          # Only Class::new is documented as such,
+          # all other Class::foo are really Class#foo
+          if bound_self == cls && (bound_self != Class || meth == :new)
+            query = "#{cls}::#{meth}"
+          else
+            query = "#{cls}##{meth}"
+          end
+          query = Magic::Help.postprocess(query)
+          return query
+        end
+      elsif !args.empty?
+        res = args[0]
+      else
+        # No block, no arguments
+        return
+      end
+
+      query = case res
+      when Module
+          res.to_s
+      when UnboundMethod, Method
+          help_method_extract(res)
+      when /\A(.*)(#|::|\.)(.*)\Z/
+        cp, k, m = $1, $2, $3
+        #puts "help for string : <#{cp}> <#{k}> <#{m}>"
+        begin
+          # For multielement paths like File::Stat const_get must be
+          # called multiple times, that is:
+          # Object.const_get("File").const_get("Stat")
+          cls = cp.split(/::/).inject(Object){|c, path_elem| c.const_get(path_elem) }
+          #puts "Const is: <#{c.inspect}>"
+          case k
+          when "#"
+            m = cls.instance_method(m)
+            m = help_method_extract(m)
+          when "::"
+            m = cls.method(m)
+            # Make sure a module method is returned
+            # It fixes `Class::new' resolving to `Class#new'
+            # (Class::new and Class#new are the same thing,
+            # but their documentations are different)
+            m = help_method_extract(m)
+            m = m.sub(/\#/, "::") if cls == Class && m == "Class#new"
+          when "."
+            begin
+              m = cls.instance_method(m)
+            rescue NameError
+              m = cls.method(m)
+            end
+            m = help_method_extract(m)
+          end
+          Magic::Help.postprocess(m, res)
+        rescue NameError
+          res
+        end
+      when String
+        res
+      else
+        res.class.to_s
+      end
+      return query
+    end
   end
 end
 
@@ -75,170 +237,9 @@ end
 #  help { Array }       - help on Array
 #  help { [] }          - help on Array
 #  help { Dir["*"] }    - help on Dir::[]
-def help(*args)
-  raise ArgumentError, "wrong number of arguments (#{args.size} for 1)" if args.size > 1
-  raise ArgumentError, "help cannot take both arguments and block" if args.size > 0 and block_given?
-
-  if block_given?
-    call_event = nil
-    res = nil
-    done = false
-    res_mm = nil
-    argument_error = false
-    base_level_module = (RUBY_VERSION > '1.9' ? BasicObject : Kernel)
-
-    # We want to capture calls to method_missing too
-    original_method_missing = base_level_module.instance_method(:method_missing)
-    base_level_module.class_eval {
-      def method_missing(*args)
-        throw :magically_irb_helped, [self, *args]
-      end
-    }
-
-    tf = lambda{|*xargs|
-      return if done
-      event = xargs[0]
-      if argument_error
-        if event == 'return'
-          done = true
-          # For functions called with wrong number of arguments,
-          # call event is not generated (function is never called),
-          # but the return event is !
-          call_event = xargs
-          throw :magically_irb_helped
-        end
-      elsif event == 'call' or event == 'c-call'
-        call_event = xargs
-        if call_event.values_at(0, 3, 5) == ['c-call', :new, Class] and
-          eval("self", call_event[4]) == ArgumentError
-          argument_error = true
-        else
-          done = true
-          # Let Kernel#method_missing run, otherwise throw
-          unless call_event.values_at(0, 3, 5) == ['call', :method_missing, base_level_module]
-            throw :magically_irb_helped
-          end
-        end
-      end
-    }
-    res_mm = catch(:magically_irb_helped) {
-      set_trace_func tf
-      res = yield()
-      done = true
-      nil
-    }
-    done = true
-    set_trace_func nil
-    base_level_module.instance_eval {
-      define_method(:method_missing, original_method_missing)
-      # It was originally private, restore it as such
-      private :method_missing
-    }
-    # Handle captured method_missing
-
-    if res_mm
-      # This is explicit Foo#method_missing:
-      # It shouldn't really be allowed as it's private.
-      if res_mm.size == 1
-        query = "Kernel#method_missing"
-        irb_help query
-        return
-      else
-        bound_self, meth = *res_mm
-        # method_missing is called if:
-        # * there was no such method
-        # * there was a method, but a private one only
-        begin
-          # Surprise ! A private method !
-          m = bound_self.method(meth)
-          query = help_method_extract(m)
-        rescue NameError
-          # No such method
-          if bound_self.is_a? Class
-            query = "#{bound_self}::#{meth}"
-          else
-            query = "#{bound_self.class}.#{meth}"
-          end
-        end
-      end
-      query = Magic::Help.postprocess(query)
-      irb_help query
-      return
-    # Handle normal call events
-    elsif call_event
-      meth, bind, cls = call_event[3], call_event[4], call_event[5]
-      bound_self = eval('self', bind)
-      if meth == :new
-        #puts "Warning: Class.new called: #{call_event.inspect}"
-        #puts "self is #{bound_self}"
-        cls = bound_self
-      end
-
-      # Foo::bar and Foo#bar look the same
-      # Check whether self == Foo to tell them apart
-      #
-      # Only Class::new is documented as such,
-      # all other Class::foo are really Class#foo
-      if bound_self == cls && (bound_self != Class || meth == :new)
-        query = "#{cls}::#{meth}"
-      else
-        query = "#{cls}##{meth}"
-      end
-      query = Magic::Help.postprocess(query)
-      irb_help query
-      return
-    end
-  elsif !args.empty?
-    res = args[0]
-  else
-    # No block, no arguments
-    return
-  end
-
-  query = case res
-  when Module
-      res.to_s
-  when UnboundMethod, Method
-      help_method_extract(res)
-  when /\A(.*)(#|::|\.)(.*)\Z/
-    cp, k, m = $1, $2, $3
-    #puts "help for string : <#{cp}> <#{k}> <#{m}>"
-    begin
-      # For multielement paths like File::Stat const_get must be
-      # called multiple times, that is:
-      # Object.const_get("File").const_get("Stat")
-      cls = cp.split(/::/).inject(Object){|c, path_elem| c.const_get(path_elem) }
-      #puts "Const is: <#{c.inspect}>"
-      case k
-      when "#"
-        m = cls.instance_method(m)
-        m = help_method_extract(m)
-      when "::"
-        m = cls.method(m)
-        # Make sure a module method is returned
-        # It fixes `Class::new' resolving to `Class#new'
-        # (Class::new and Class#new are the same thing,
-        # but their documentations are different)
-        m = help_method_extract(m)
-        m = m.sub(/\#/, "::") if cls == Class && m == "Class#new"
-      when "."
-        begin
-          m = cls.instance_method(m)
-        rescue NameError
-          m = cls.method(m)
-        end
-        m = help_method_extract(m)
-      end
-      Magic::Help.postprocess(m, res)
-    rescue NameError
-      res
-    end
-  when String
-    res
-  else
-    res.class.to_s
-  end
-  irb_help query
+def help(*args, &block)
+  query = Magic::Help.resolve_help_query(*args, &block)
+  irb_help(query) if query
 end
 
 def help_method_extract(m) # :nodoc:
